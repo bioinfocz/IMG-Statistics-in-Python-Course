e8cccf8aef618d04b5ab0866b89c667487300133
jakubovciak <jan.kubovciak@img.cas.cz>
2026-05-25 16:33:07 +0200

    01 final version

--- solutions/solution_01_03.py
+++ solutions/solution_01_03.py
@@ -1,8 +1,12 @@
+plt.rc("font", size=10)
 f, axes = plt.subplots( 1, 2, figsize=(10, 7) )
-sns.boxplot( x = '60+ y.o.', y= 'canton name' ,data=dfFractions , ax=axes[0]).set_title('(A) 60+ y.o. per canton')
-sns.histplot( hue = 'majority language' , x='Catholic' , data=dfFractions, multiple='dodge', ax=axes[1]).set_title('(B) Catholic prop. per language')
+sns.boxplot( x = '60+ y.o.', y= 'canton name' ,data=dfFractions , color="royalblue",ax=axes[0]).set_title('(A) 60+ y.o. per canton')
+axes[0].axvline(dfFractions['60+ y.o.'].mean(),color='g', linestyle='--' , label = "mean")
+axes[0].legend()
+sns.histplot( hue = 'majority language' , x='Catholic' , data=dfFractions, multiple='dodge', ax=axes[1])
+axes[1].set_title('(B) Catholic prop. per language')
+sns.move_legend(axes[1], "upper right")
 plt.tight_layout()# this makes the panels margins and setup more graceful
-
 
 # save PDF, dimensions follow plot
 f.savefig('./exC.pdf')
